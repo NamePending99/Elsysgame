79c8d64ec90a455f89518854833b46beb5419b0f
ChristofferLeo <92925654+ChristofferLeo@users.noreply.github.com>
2021-10-22 12:45:55 +0200

    Update minigame.py

--- minigame.py
+++ minigame.py
@@ -1,4 +1,8 @@
 import RPi.GPIO as GPIO
-x = "Hei OV"
-print(x)
-print('Itgk')
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True:
+    if GPIO.input(10) == GPIO.HIGH:
+        print('Knapp ble trykket på!')
